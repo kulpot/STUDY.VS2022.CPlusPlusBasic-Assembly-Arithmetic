b708891ced5a1931e6eca5fbace3961830da81f5
Kulpot <kulpotwahu@gmail.com>
2023-10-08 14:32:59 +0800

    Add project files.

---
+++ Basic Assembly Arithmetic/Mainer.cpp
@@ -0,0 +1,13 @@
+
+//ref link:https://www.youtube.com/watch?v=eUXUFOAUAns&list=PLRwVmtr-pp05c1HTBj1no6Fl6C6mlxYDG&index=5
+//
+
+// MyAssembly.asm - (customBuildTool)commandline - ml /c /Cx /cof "%(FullPath)"
+// MyAssembly.asm - (customBuildTool)output - %(Filename).obj
+
+extern "C" void doit();
+
+void main()
+{
+	doit();
+}
---
+++ Basic Assembly Arithmetic/MyAssembly.asm
@@ -0,0 +1,16 @@
+
+.586
+.model flat, c
+.stack 100h
+.data
+
+.code
+
+doit proc
+
+	mov eax, 8
+	mov ebx, eax
+
+doit endp
+
+end
