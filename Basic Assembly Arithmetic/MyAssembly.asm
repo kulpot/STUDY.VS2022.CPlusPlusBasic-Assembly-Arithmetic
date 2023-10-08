0df427c63115298fb04d813f5284973104d5524b
Kulpot <kulpotwahu@gmail.com>
2023-10-08 15:25:43 +0800

    ...

--- Basic Assembly Arithmetic/MyAssembly.asm
+++ Basic Assembly Arithmetic/MyAssembly.asm
@@ -9,9 +9,19 @@
 doit proc
 
 	; this is a comment i can type whatever i like after a semicolon and assembler will ignore it
+	; 8 + 4 - 2 + 3 = 13
 
 	mov eax, 8
-	mov ebx, eax
+	mov ebx, 4
+	mov ecx, 2
+	mov edx, 3
+
+	add ebx, eax
+	sub ebx, ecx
+	add edx, ebx
+
+	;mov eax, 8
+	;mov ebx, eax
 
 doit endp
 
